2f13535636a6735b76287da54a294b361923c89c
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-01-08 12:16:31 -0300

    feat: inicio do projeto financas estrutura e recursos iniciais

---
+++ src/main/java/br/com/code7/financasbackend/FinancasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.code7.financasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/code7/financasbackend/FinancasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.code7.financasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
