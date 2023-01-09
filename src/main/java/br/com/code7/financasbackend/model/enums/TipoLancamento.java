4dd9d08363cc4cc99f971201696cb868f1ee058d
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-01-08 22:49:27 -0300

    feat: adicionando tipo de lancamentos

---
+++ src/main/java/br/com/code7/financasbackend/model/enums/TipoLancamento.java
@@ -0,0 +1,21 @@
+package br.com.code7.financasbackend.model.enums;
+
+public enum TipoLancamento {
+
+	PEN("PENDENTE"), CAN("CANCELADO"), FAT("FATURADO"), PAG("EFETIVADO");
+
+	private final String nome;
+
+	TipoLancamento(String nome) {
+		this.nome = nome;
+	}
+
+	public String descricao() {
+		return nome;
+	}
+
+	public String toString() {
+		return nome;
+	}
+
+}
