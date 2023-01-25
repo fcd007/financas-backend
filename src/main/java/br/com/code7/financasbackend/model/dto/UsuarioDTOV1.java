b337068fb32429ae43b3f6b52913da1ca1c267b8
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-01-25 07:12:10 -0300

    refact: ajuste de mapeamento de dto usuario

--- src/main/java/br/com/code7/financasbackend/model/dto/UsuarioDTOV1.java
+++ src/main/java/br/com/code7/financasbackend/model/dto/UsuarioDTOV1.java
@@ -1,11 +1,21 @@
 package br.com.code7.financasbackend.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UsuarioDTOV1 {
+
+	private Long id;
 
 	private String nome;
 
