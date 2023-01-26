d569bccff024a22524a9049e2280e9b20d6ae8f9
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-01-25 21:08:19 -0300

    feat: revisando o mapper v1

--- src/main/java/br/com/code7/financasbackend/core/usuario/UsuarioMapperV1.java
+++ src/main/java/br/com/code7/financasbackend/core/usuario/UsuarioMapperV1.java
@@ -4,28 +4,20 @@ import br.com.code7.financasbackend.model.dto.UsuarioDTOV1;
 import br.com.code7.financasbackend.model.entity.Usuario;
 
 public class UsuarioMapperV1 {
-	
-	 // Converter Usuario JPA Entity para usuarioDTOV1	
-	public static UsuarioDTOV1 mapToUserDto(Usuario usuario){
-		UsuarioDTOV1 usuarioDto = new UsuarioDTOV1(
-				usuario.getId(),
-				usuario.getNome(),
-				usuario.getEmail(),
-				usuario.getSenha()
-        );
-		
-        return usuarioDto;
-    }
-	
+
+	// Converter Usuario JPA Entity para usuarioDTOV1
+	public static UsuarioDTOV1 mapToUserDto(Usuario usuario) {
+		UsuarioDTOV1 usuarioDto = new UsuarioDTOV1(usuario.getId(), usuario.getNome(), usuario.getEmail(),
+				usuario.getSenha());
+
+		return usuarioDto;
+	}
+
 	// Converter usuarioDTOV1 para Usuario JPA Entity
-	public static Usuario mapToUser(UsuarioDTOV1 usuarioDto){
-		Usuario usuario = new Usuario(
-				usuarioDto.getId(),
-				usuarioDto.getNome(),
-				usuarioDto.getEmail(),
-				usuarioDto.getSenha()
-        );
-		
-        return usuario;
+	public static Usuario mapToUser(UsuarioDTOV1 usuarioDto) {
+		Usuario usuario = new Usuario(usuarioDto.getId(), usuarioDto.getNome(), usuarioDto.getEmail(),
+				usuarioDto.getSenha());
+
+		return usuario;
 	}
 }
