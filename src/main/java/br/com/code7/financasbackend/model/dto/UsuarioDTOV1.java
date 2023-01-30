85f4659120ba7f5c7a3cc1bf0a14a6cfd0fd3cdb
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-01-30 06:18:37 -0300

    refact: removendo imports

--- src/main/java/br/com/code7/financasbackend/model/dto/UsuarioDTOV1.java
+++ src/main/java/br/com/code7/financasbackend/model/dto/UsuarioDTOV1.java
@@ -2,7 +2,6 @@ package br.com.code7.financasbackend.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
