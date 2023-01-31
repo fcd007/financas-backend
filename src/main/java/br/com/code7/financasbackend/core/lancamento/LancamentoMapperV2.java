2eed6043e1aa7246d16e37837723d0251cd9cbc1
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-01-31 20:59:47 -0300

    feat: adicionando mapper mapstruct

--- src/main/java/br/com/code7/financasbackend/core/lancamento/LancamentoMapperV2.java
+++ src/main/java/br/com/code7/financasbackend/core/lancamento/LancamentoMapperV2.java
@@ -1,5 +1,17 @@
 package br.com.code7.financasbackend.core.lancamento;
 
-public class LancamentoMapperV2 {
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
+import br.com.code7.financasbackend.model.dto.LancamentoDTOV1;
+import br.com.code7.financasbackend.model.entity.Lancamento;
+
+@Mapper
+public interface LancamentoMapperV2 {
+
+	LancamentoMapperV2 MAPPER = Mappers.getMapper(LancamentoMapperV2.class);
+
+	LancamentoDTOV1 mapperLancamentoToDto(Lancamento lancamento);
+
+	Lancamento mapperDtoToLancamento(LancamentoDTOV1 dto);
 }
