f5c580da89249f6061f294e27e5d5d41b4e8ad1a
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-02-13 21:08:27 -0300

    feat: adicionado novo script de banco de dados

--- src/main/resources/script-sql/schema.sql
+++ src/main/resources/script-sql/schema.sql
@@ -1,25 +1,42 @@
-CREATE DATABASE finance_db_v1;
+CREATE DATABASE finance_db_v2;
 
-CREATE SCHEMA financas;
+CREATE SCHEMA financas2;
 
-CREATE TABLE financas.usuario
+CREATE TABLE financas2.usuario
 (
   id bigserial NOT NULL PRIMARY KEY,
   nome character varying(150),
   email character varying(100),
   senha character varying(20),
-  data_cadastro date default now()
+  ativo boolean not null default true,
+  data_criacao timestamp not null default current_timestamp,
+  data_atualizacao timestamp,
+  version bigserial NOT NULL
 );
 
-CREATE TABLE financas.lancamento
+CREATE TABLE financas2.cliente (
+	numero bigserial primary key,
+	nome varchar(128) not null,
+	email varchar(256) not null,
+	ativo boolean not null default true,
+	id_usuario bigint REFERENCES financas2.usuario (id),
+	data_criacao timestamp not null default current_timestamp,
+	data_atualizacao TIMESTAMP,
+  	version bigserial NOT NULL
+);
+
+CREATE TABLE financas2.lancamento
 (
   id bigserial NOT NULL PRIMARY KEY ,
   descricao character varying(100) NOT NULL,
+  dia integer NOT NULL,
   mes integer NOT NULL,
   ano integer NOT NULL,
   valor numeric(16,2),
   tipo character varying(20),
   status character varying(20),
-  id_usuario bigint REFERENCES financas.usuario (id),
-  data_cadastro date default now()
+  id_usuario bigint REFERENCES financas2.usuario (id),
+  data_criacao timestamp not null default current_timestamp,
+  data_atualizacao TIMESTAMP,
+  version bigserial NOT NULL
 );
