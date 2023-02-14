1b4e2be0a9ddfdfacf0f3d8b089f99d9ddffd251
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-02-14 20:53:27 -0300

    feat: ajuste de entidade

--- src/main/java/br/com/code7/financasbackend/model/entity/Usuario.java
+++ src/main/java/br/com/code7/financasbackend/model/entity/Usuario.java
@@ -1,11 +1,17 @@
 package br.com.code7.financasbackend.model.entity;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -36,5 +42,16 @@ public class Usuario {
 	@Column(name = "senha", nullable = false, unique = true)
 	@JsonIgnore
 	private String senha;
+	
+	@Version
+	private Long version;
+
+	@Column(name = "data_criacao")
+	@Convert(converter = Jsr310JpaConverters.LocalDateTimeConverter.class)
+	private LocalDateTime dataCriacao;
+	
+	@Column(name = "data_atualizacao")
+	@Convert(converter = Jsr310JpaConverters.LocalDateTimeConverter.class)
+	private LocalDateTime dataAtualizacao;
 
 }
